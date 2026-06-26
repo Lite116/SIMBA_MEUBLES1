5087c6261e23ad6a7597033fb37856a30b85a2c6
florella-yannis <florellayannis@gmail.com>
2026-06-26 23:21:18 +0200

    Update pack-options.ts

--- lib/pack-options.ts
+++ lib/pack-options.ts
@@ -20,14 +20,6 @@ export function getAllVisibleRoomOptions(): RoomOptions {
 export const ROOM_OPTIONS: RoomOptions = {
   salon: [
     {
-      id: 'salon-1',
-      name: 'Salon 1 (hors catalogue)',
-      description: 'Salon avec mobilier de base',
-      image: '/images/salons/s1.webp',
-      price: 0,
-      hidden: true,
-    },
-    {
       id: 'salon-2',
       name: 'Salon 1',
       description:
@@ -44,6 +36,62 @@ export const ROOM_OPTIONS: RoomOptions = {
       price: 0,
     },
     {
+      id: 'salon-5',
+      name: 'Salon 3',
+      description:
+        '312 cm x 142 cm. Lit et coffre dans la partie centrale. Tissu monolith noir.',
+      image: '/images/salons/s6.webp',
+      price: 0,
+    },
+    {
+      id: 'salon-8',
+      name: 'Salon 4',
+      description:
+        '320 cm x 160 cm. Simili noir et tissu gris. Lit et coffre dans partie centrale.',
+      image: '/images/salons/s9.webp',
+      price: 0,
+    },
+    {
+      id: 'salon-11',
+      name: 'Salon 5',
+      description:
+        '240 cm x 160 cm. Lit et coffre dans la partie centrale. Méridienne réversible.',
+      image: '/images/salons/s12.webp',
+      price: 0,
+    },
+    {
+      id: 'salon-12',
+      name: 'Salon 6',
+      description:
+        '237 cm x 142 cm. Méridienne réversible. Lit et coffre dans partie centrale, tissu velours côtelé.',
+      image: '/images/salons/s13.webp',
+      price: 0,
+    },
+    {
+      id: 'salon-14',
+      name: 'Salon 7',
+      description:
+        '215 cm x 127 cm avec lit dans la partie centrale et coffre dans la méridienne. Tissu noir lisse.',
+      image: '/images/salons/s15.webp',
+      price: 0,
+    },
+    {
+      id: 'salon-15',
+      name: 'Salon 8',
+      description:
+        '215 cm x 127 cm avec lit dans la partie centrale et coffre dans la méridienne. Tissu beige lisse.',
+      image: '/images/salons/s16.webp',
+      price: 0,
+    },
+    {
+      id: 'salon-1',
+      name: 'Salon 1 (hors catalogue)',
+      description: 'Salon avec mobilier de base',
+      image: '/images/salons/s1.webp',
+      price: 0,
+      hidden: true,
+    },
+    {
       id: 'salon-4',
       name: 'Salon 3',
       description:
@@ -51,14 +99,6 @@ export const ROOM_OPTIONS: RoomOptions = {
       image: '/images/salons/s5.webp',
       price: 0,
       hidden: true,
-    },
-    {
-      id: 'salon-5',
-      name: 'Salon 4',
-      description:
-        '312 cm x 142 cm. Lit et coffre dans la partie centrale. Tissu monolith noir.',
-      image: '/images/salons/s6.webp',
-      price: 0,
     },
     {
       id: 'salon-6',
@@ -77,14 +117,6 @@ export const ROOM_OPTIONS: RoomOptions = {
       hidden: true,
     },
     {
-      id: 'salon-8',
-      name: 'Salon 5',
-      description:
-        '320 cm x 160 cm. Simili noir et tissu gris. Lit et coffre dans partie centrale.',
-      image: '/images/salons/s9.webp',
-      price: 0,
-    },
-    {
       id: 'salon-9',
       name: 'Salon 9',
       description: 'Salon supplémentaire standard',
@@ -101,44 +133,12 @@ export const ROOM_OPTIONS: RoomOptions = {
       hidden: true,
     },
     {
-      id: 'salon-11',
-      name: 'Salon 6',
-      description:
-        '240 cm x 160 cm. Lit et coffre dans la partie centrale. Méridienne réversible.',
-      image: '/images/salons/s12.webp',
-      price: 0,
-    },
-    {
-      id: 'salon-12',
-      name: 'Salon 7',
-      description:
-        '237 cm x 142 cm. Méridienne réversible. Lit et coffre dans partie centrale, tissu velours côtelé.',
-      image: '/images/salons/s13.webp',
-      price: 0,
-    },
-    {
       id: 'salon-13',
       name: 'Salon 13 (hors catalogue)',
       description: 'Salon supplémentaire standard',
       image: '/images/salons/s14.webp',
       price: 0,
       hidden: true,
-    },
-    {
-      id: 'salon-14',
-      name: 'Salon 8',
-      description:
-        '215 cm x 127 cm avec lit dans la partie centrale et coffre dans la méridienne. Tissu noir lisse.',
-      image: '/images/salons/s15.webp',
-      price: 0,
-    },
-    {
-      id: 'salon-15',
-      name: 'Salon 9',
-      description:
-        '215 cm x 127 cm avec lit dans la partie centrale et coffre dans la méridienne. Tissu beige lisse.',
-      image: '/images/salons/s16.webp',
-      price: 0,
     },
   ],
   'salle-a-manger': [
@@ -187,6 +187,27 @@ export const ROOM_OPTIONS: RoomOptions = {
   ],
   chambre: [
     {
+      id: 'chambre-6',
+      name: 'Chambre 1',
+      description: 'Lit 160 cm. Garde robe 4 portes 218 cm.',
+      image: '/images/chambres/chambree1.webp',
+      price: 0,
+    },
+    {
+      id: 'chambre-7',
+      name: 'Chambre 2',
+      description: 'Lit 160 cm. Garde robe portes coulissantes 200 cm.',
+      image: '/images/chambres/chambree.webp',
+      price: 0,
+    },
+    {
+      id: 'chambre-3',
+      name: 'Chambre 3',
+      description: 'Lit 160 cm. Garde Robe 225 cm.',
+      image: '/images/chambres/chambre3.webp',
+      price: 0,
+    },
+    {
       id: 'chambre-1',
       name: 'Chambre 1',
       description: 'Lit 160 cm. Garde Robe 220,5 cm x 203 cm H.',
@@ -203,13 +224,6 @@ export const ROOM_OPTIONS: RoomOptions = {
       hidden: true,
     },
     {
-      id: 'chambre-3',
-      name: 'Chambre 3',
-      description: 'Lit 160 cm. Garde Robe 225 cm.',
-      image: '/images/chambres/chambre3.webp',
-      price: 0,
-    },
-    {
       id: 'chambre-4',
       name: 'Chambre 4 (hors catalogue)',
       description: 'Lit 160 cm. Garde Robe 240 cm x 215 cm H.',
@@ -223,20 +237,7 @@ export const ROOM_OPTIONS: RoomOptions = {
       description: 'Lit 160 cm. Garde Robe 250 cm x 218 cm H.',
       image: '/images/chambres/chambre5.webp',
       price: 0,
-    },
-    {
-      id: 'chambre-6',
-      name: 'Chambre 1',
-      description: 'Lit 160 cm. Garde robe 4 portes 218 cm.',
-      image: '/images/chambres/chambree1.webp',
-      price: 0,
-    },
-    {
-      id: 'chambre-7',
-      name: 'Chambre 2',
-      description: 'Lit 160 cm. Garde robe portes coulissantes 200 cm.',
-      image: '/images/chambres/chambree.webp',
-      price: 0,
+      hidden: true,
     },
   ]
 };
